eab4bbcc4c9c27aff4b02e56af95116b60e3fd4a
Taras Kulyk <tk@div-art.com>
2017-12-03 13:27:00 +0200

    c

--- resources/assets/js/admin/admin.js
+++ resources/assets/js/admin/admin.js
@@ -9,31 +9,36 @@ Vue.use(BootstrapVue);
 let publicPages = ['/pages/login'];
 
 router.beforeEach((to, from, next) => {
-    let authUser = sessionStorage.getItem('authUser');
-    if (authUser) {
-        next();
-    } else {
-        //$http.get('auth.user').then('response' => ());
-        $.ajax({
-           url: 'test'
-        });
-    }
-    //console.log(JSON.parse(sessionStorage.getItem('user')));
-    console.log(sessionStorage.user);
-    if(publicPages.includes(to.fullPath)) {
-        sessionStorage.setItem('user', JSON.stringify({
-            asd: 'zxczxc'
-        }));
-        next();
-    } else {
-        router.push('/pages/login');
-    }
+    //let authUser = sessionStorage.getItem('authUser');
+    //console.log(authUser);
+    //console.log('test');
+    next();
+
+//    if (authUser) {
+//        next();
+//    } else {
+//        //$http.get('auth.user').then('response' => ());
+//        /*$.ajax({
+//           url: 'test'
+//        })*/;
+//        router.push('/pages/login');
+//    }
+//    //console.log(JSON.parse(sessionStorage.getItem('user')));
+//    //console.log(sessionStorage.user);
+//    if(publicPages.includes(to.fullPath)) {
+//        /*sessionStorage.setItem('user', JSON.stringify({
+//            asd: 'zxczxc'
+//        }));*/
+//        next();
+//    } else {
+//        router.push('/pages/login');
+//    }
 });
 
 new Vue({
-	el: '#app',
+    el: '#app',
     template: '<App/>',
-	router,
+    router,
     components: {
         App
     }
